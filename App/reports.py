71efc3ca7d82f68a59c0a54608b87bfdc711d873
AngieSAponte <90227932+AngieSAponte@users.noreply.github.com>
2025-02-27 09:11:40 -0500

    Función consulta microinversores
    Se agrega la función para consultar la lista de microinversores por id plant. Se inicia la función para consultar información por cada microinversor.

--- App/reports.py
+++ App/reports.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 import logging
 import requests
 import time
+from datetime import datetime
 
 from utils.configHandler import ConfigHandler
 
@@ -40,13 +41,14 @@ class OnomondoReport:
 
                 if response.status_code != 200:
                     print(f"Error {response.status_code}: {response.text}")
+                    self.logger.error(f"Error {response.status_code}: {response}")
                     break  
 
                 data = response.json()
                 
                 if data["status"] != "0":
                     # print("Error en la consulta:", data["message"])
-                    self.logger.error(f"Error in the consult: {data["message"]}")
+                    self.logger.error(f"Error in the consult: {data['message']}")
                     break
 
                 print("Datos recibidos:", data)
@@ -91,21 +93,76 @@ class OnomondoReport:
         micros_per_plant = self.config_data.get_specified_plant()
         key = self.key
         url = self.config_data.get_url() + micros_per_plant + "key=" + key
+        print(url)
+
+        headers = {
+        "Content-Type": "application/json",
+        "Accept": "*/*"
+        }
 
         all_plants = self.get_list_plants()
 
-        for plant in all_plants:
-            plant.get("id_plant")
-            print("Plants ID", plant)
+        try:
+             
+            for plant in all_plants:
+                proy_id = plant.get("id_plant")
+                print("Plants ID", proy_id)
 
-            if not plant:
-                self.logger.info("There are no more plants IDs.")
-                break
+                if not plant:
+                    self.logger.info("There are no more plants IDs.")
+                    break
+
+                data_req = {"id": proy_id} 
+                response = requests.post(url, headers=headers, json=data_req)
+
+                if response.status_code != 200:
+                        print(f"Error {response.status_code}: {response.text}")
+                        self.logger.error(f"Error {response.status_code}: {response}")
+                        break  
+
+                data = response.json()
+                
+                if data["status"] != "0":
+                        # print("Error en la consulta:", data["message"])
+                        self.logger.error(f"Error in the consult: {data['message']}")
+                        break
+                
+                time.sleep(6)
+
+                micro_datas = {item.get("mi_sn") for item in data.get("data", {}).get("micro_datas", [])}
+
+                print("Lista de microinversores: ", micro_datas)
+
+                all_microinverters.append({"id_plant": proy_id, "plant_name": plant.get("plant_name"), "micros_id": micro_datas})
+        
+        except Exception as ex:
+            self.logger.error(f"Error while asking for plants {ex}")
+            return []
+        
+        print("SERIALES DE LOS MICROS: ", all_microinverters)
+
+        return all_microinverters
 
     def get_data_microinverters_per_plant(self) -> list:
-        self.logger.info(
-            f"Error en AnotherClass{self.config_data.get_specified_plant()}"
-        )
+        all_data_microinverters = []
+        current_date = datetime.now()
+        current_date_formatted = current_date.strftime("%Y-%m-%d")
+        micros_per_plant = self.config_data.get_data_microinverter()
+        key = self.key
+        url = self.config_data.get_url() + micros_per_plant + "key=" + key
+        print(url)
+        print("la fecha actual es:", current_date_formatted)
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "*/*"
+            }
+        
+        all_microinverters = self.get_list_microinverters_per_plant()
+
+        # { 
+        # "station_id": 1325870 , 
+        # "date": "2025-02-25", 
+        # "sn": "106154401367" }
 
     def information_processing(self) -> None:
         self.logger.info(
