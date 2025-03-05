2bb10f58aad139d6a9c2128db973d65955ad07ac
jmarulandaerco <josemarulanda63@hotmail.com>
2025-03-05 11:07:15 -0500

    Correciones con lo referente a reintentos y optimización de la parte de reintentos

--- App/reports.py
+++ App/reports.py
@@ -29,7 +29,6 @@ class HoymileReport:
         get_plant_list = self.config_data.get_plant_list()
         key = self.key
         url = self.config_data.get_url() + get_plant_list + "key=" + key
-        #url = f"https://wapi.hoymiles.com{get_plant_list}key={key}"
 
         headers = {
             "Content-Type": "application/json",
@@ -37,66 +36,55 @@ class HoymileReport:
         }
         
         try:
-
             while next_page is not None:  
                 data_req = {"next": next_page}  
                 print(f"\n Enviando solicitud: {data_req}")
-                for attempt in range(1,  self.MAX_RETRIES + 1):
 
+                for attempt in range(1, self.MAX_RETRIES + 1):
                     response = requests.post(url, headers=headers, json=data_req)
 
                     if response.status_code != 200:
                         print(f"Error {response.status_code}: {response.text}")
                         self.logger.error(f"Error {response.status_code}: {response}")
-                        break  
+                        time.sleep(6)  
+                        continue  
 
                     data = response.json()
                     
                     if data["status"] != "0":
-                        # print("Error en la consulta:", data["message"])
-                        self.logger.error(f"Error in the consult: {data['message']}")
-                        break
+                        self.logger.error(f"Error in the consult: {data['message']} with status {data["status"]}")
+                        time.sleep(6)  
+                        continue 
 
                     print("Datos recibidos:", data)
 
-                    # Obtener lista de estaciones
+                    
                     stations = data.get("data", {}).get("stations", [])
                     
                     for entry in stations:
-                        
-                    #ids = [station.get("id") for station in data.get("data", {}).get("stations", [])]
-                        all_plants.append({"id_plant": entry.get("id"), "plant_name":entry.get("station_name")})
-                    #all_plants.extend(stations)  # Agregar solo si hay estaciones
-                    #proy_id.extend(ids)
-                    #print("ESTOS SON LOS IDS DE LAS PLANTAS:", proy_id)
+                        all_plants.append({"id_plant": entry.get("id"), "plant_name": entry.get("station_name")})
 
-                    
-                    next_page = data.get("data", {}).get("next", None)  # Si no hay "next", devuelve None
-                    
+                    next_page = data.get("data", {}).get("next", None)  
                     print(f"Next page actualizado a: {next_page}")
 
-                    
                     if not stations or next_page is None:
                         print("No hay más estaciones o `next` no existe. bye.")
                         self.logger.info("There are no more stations available. Bye")
-                        break
+                        return all_plants  
                     
                     time.sleep(6)
-                    break
-                
-                if attempt ==  self.MAX_RETRIES:
+                    break  
+
+                else:  
                     print("Se alcanzó el número máximo de intentos sin éxito.")
-                    self.logger.error(f"Unable to consult the list of plants, maximum number of attempts made. Max retries ={self.MAX_RETRIES}")
+                    self.logger.error(f"Unable to consult the list of plants, maximum number of attempts made. Max retries = {self.MAX_RETRIES}")
                     return []
-                print(f"\n Total de estaciones obtenidas: {len(all_plants)}")
-            print("Lista filtrada: ", all_plants)
-        
+
         except Exception as ex: 
             self.logger.error(f"Error while asking for plants {ex}")
             return []
 
         return all_plants
-
 
     def get_list_microinverters_per_plant(self) -> list:
         
@@ -119,9 +107,9 @@ class HoymileReport:
                 proy_id = plant.get("id_plant")
                 print("Plants ID", proy_id)
 
-                if not plant:
-                    self.logger.info("There are no more plants IDs.")
-                    break
+                # if not plant:
+                #     self.logger.info("There are no more plants IDs.")
+                #     break
 
                 data_req = {"id": proy_id} 
                 for attempt in range(1,  self.MAX_RETRIES + 1):
@@ -130,18 +118,20 @@ class HoymileReport:
                     if response.status_code != 200:
                             print(f"Error {response.status_code}: {response.text}")
                             self.logger.error(f"Error {response.status_code}: {response}")
-                            break  
+                            time.sleep(6)  
+                            continue  
 
                     data = response.json()
                     
                     if data["status"] != "0":
                             # print("Error en la consulta:", data["message"])
-                            self.logger.error(f"Error in the consult: {data['message']}")
-                            break
+                            self.logger.error(f"Error in the consult: {data['message']} with status {data["status"]}")
+                            time.sleep(6)  
+                            continue
                     
                     time.sleep(6)
                     break
-                if attempt ==  self.MAX_RETRIES:
+                else:
                     print("Se alcanzó el número máximo de intentos sin éxito.")
                     self.logger.error(f"Unable to consult the list of plants, maximum number of attempts made. Max retries ={self.MAX_RETRIES}")
                     return []
