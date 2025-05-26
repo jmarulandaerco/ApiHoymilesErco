d60ceebd2db0f40b6181a4ef14cb64d1e181ed89
josemarulanda80 <josemarulanda63@hotmail.com>
2025-05-26 13:41:57 -0500

    Se arregla daño

--- utils/logger.py
+++ utils/logger.py
@@ -20,29 +20,29 @@ class LoggerHandler:
     """
     name: str = __name__
 
-def __post_init__(self):
-    helper = HelperReport()
-    helper.check_log_sizes()
-    self.logger = logging.getLogger(self.name)
-    self.logger.setLevel(logging.INFO)
+    def __post_init__(self):
+        helper = HelperReport()
+        helper.check_log_sizes()
+        self.logger = logging.getLogger(self.name)
+        self.logger.setLevel(logging.INFO)
 
-    # ✅ Evita agregar múltiples veces los mismos handlers
-    if not self.logger.handlers:
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.INFO)
+        # ✅ Evita agregar múltiples veces los mismos handlers
+        if not self.logger.handlers:
+            console_handler = logging.StreamHandler()
+            console_handler.setLevel(logging.INFO)
 
-        configHandler = ConfigHandler("config.ini")
-        file_handler = logging.FileHandler(
-            str(configHandler.get_name_log()), mode="a")
-        file_handler.setLevel(logging.INFO)
+            configHandler = ConfigHandler("config.ini")
+            file_handler = logging.FileHandler(
+                str(configHandler.get_name_log()), mode="a")
+            file_handler.setLevel(logging.INFO)
 
-        formatter = logging.Formatter(
-            "%(asctime)s - %(levelname)s - %(message)s")
-        console_handler.setFormatter(formatter)
-        file_handler.setFormatter(formatter)
+            formatter = logging.Formatter(
+                "%(asctime)s - %(levelname)s - %(message)s")
+            console_handler.setFormatter(formatter)
+            file_handler.setFormatter(formatter)
 
-        self.logger.addHandler(console_handler)
-        self.logger.addHandler(file_handler)
-def get_logger(self):
-    return self.logger
+            self.logger.addHandler(console_handler)
+            self.logger.addHandler(file_handler)
+    def get_logger(self):
+        return self.logger
 
